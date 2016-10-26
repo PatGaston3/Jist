4504d64565deb36a791914ab644ef5ea604dc2e3
AnnieFisher <annielynn.fisher@gmail.com>
2016-10-26 11:18:08 -0600

    jobsController and DAO complete

--- JistMVC/src/controllers/JobsController.java
+++ JistMVC/src/controllers/JobsController.java
@@ -1,31 +1,109 @@
 package controllers;
 
+import java.util.Collection;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import data.JobsDAO;
+import entities.Job;
+import entities.User;
 
 @RestController
 public class JobsController {
 
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private JobsDAO jobsDAO;
-	
+
 	@RequestMapping(path = "ping", method = RequestMethod.GET)
 	public String ping() {
 		return "pong";
 	}
+
+	@RequestMapping(path = "user", method = RequestMethod.GET)
+	public List<User> indexUsers() {
+		return jobsDAO.indexUsers();
+	}
+
+	@RequestMapping(path = "user/{id}", method = RequestMethod.GET)
+	public User showUser(@PathVariable int id) {
+		return jobsDAO.showUser(id);
+	}
+
+	@RequestMapping(path = "user/{id}/job", method = RequestMethod.GET)
+	public Collection<Job> showJob(@PathVariable int id) {
+		return jobsDAO.showJob(id);
+	}
+
+	@RequestMapping(path = "user/{id}", method = RequestMethod.PUT)
+	public void updateUser(@PathVariable int id, @RequestBody String userJSON) {
+		ObjectMapper mapper = new ObjectMapper();
+		User user = null;
+		try {
+			user = mapper.readValue(userJSON, User.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		jobsDAO.update(id, user);
+	}
+
+	@RequestMapping(path = "user/{id}/job/{id}", method = RequestMethod.PUT)
+	public void updateJobs(@PathVariable int id, @RequestBody String jobJSON) {
+		ObjectMapper mapper = new ObjectMapper();
+		Job job = null;
+		try {
+			job = mapper.readValue(jobJSON, Job.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		jobsDAO.update(id, job);
+	}
+
+	@RequestMapping(path = "user", method = RequestMethod.POST)
+	public void createUser(@RequestBody String userJSON, HttpServletResponse res) {
+		ObjectMapper mapper = new ObjectMapper();
+		User user = null;
+		try {
+			user = mapper.readValue(userJSON, User.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		res.setStatus(201);
+		jobsDAO.create(user);
+	}
+
+	@RequestMapping(path = "user/{id}/job", method = RequestMethod.POST)
+	public void createJob(@PathVariable int id, @RequestBody String userJSON) {
+		ObjectMapper mapper = new ObjectMapper();
+		Job job = null;
+		try {
+			job = mapper.readValue(userJSON, Job.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		jobsDAO.createJob(id, job);
+	}
 	
-//	@RequestMapping(path = "user", method = RequestMethod.GET)
-//	public List<User> indexUsers() {
-//		
-//	}
+	@RequestMapping(path = "user/{id}", method = RequestMethod.DELETE)
+	public void destroyUser(@PathVariable int id) {
+		jobsDAO.destroyUser(id);
+	}
+	
+	@RequestMapping(path = "user/{id}/job/{jID}", method = RequestMethod.DELETE) 
+	public void destroytJob(@PathVariable int id, @PathVariable int jId) {
+		jobsDAO.destroyJob(id,  jId);
+	}
 }
--- JistMVC/src/data/JobsDAO.java
+++ JistMVC/src/data/JobsDAO.java
@@ -7,11 +7,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
-//import entities.Job;
-//import.entities.User;
+
+import entities.Job;
+import entities.User;
+
 
 @Transactional
 public class JobsDAO {
@@ -22,82 +23,101 @@ public class JobsDAO {
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
 	
-//	public List<User> indexUsers() {
-//		String query = "Select u from User u";
-//		return em.createQuery(query, User.class).getResultList();
-//	}
-//	
-//	public List<Job> indexJobs() {
-//		String query = "Select j from Job j";
-//		return em.createQuery(query, Job.class).getResulList();
-//	}
-//		
-//	public User showUser(int id) {
-//		return em.find(User.class, id);
-//	} 
-//	
-//	public Collection<Job> showJob(int id) {
-//		User user = null;
-//		try{
-//			user = em.find(User.class, id);
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//		if(user != null) {
-//			return user.getJob();
-//		}
-//		return null;
-//	}
-//	
-//	public void createJob(int id, Job job) {
-//		User user = em.find(User.class, id);
-//		job.setUser(user);
-//		em.persist(job);
-//		em.flush();
-//	}
-//	public User create (User user) {
-//		String rawPassword = user.getPassword();
-//		String encodedPassword = passwordEncoder.encode(rawPassword);
-//		user.getPassword(encodedPassword);
-//		em.persist(user);
-//		em.flush();
-//		return user;	
-//	}
-//	public User authenticateUser (User loginData) throws Exception {
-//		User user = em.createQuery("select u from User u where u.username = :username", User.class)
-//					.setParameter("username", loginData.getUsername())
-//					.getSingleResult();
-//		if(passwordEncoder.matches(loginData.getPassword(), user.getPassword())) {
-//			return null;
-//		}
-//		return null;
-//	}
-//	
-//	public User update(int id, User user) {
-//		User updatedUser = em.find(User.class, id);
-//		updatedUser.setUsername(user.getUsername());
-//		updatedUser.setPassword(user.getPassword());
-//		updatedUser.setEmail(user.getEmail());
-//		updatedUser.setJobs(User.getJobs());
-//		em.merge(updatedUser);
-//		return updatedUser;
-//	}
-//	
-//	public Job update(int id, Job job) {
-//		User managedUser = em.find(User.class, id);
-//		job.setUser(managedUser);
-//		Job managedJob = em.find(Job.class, id);
-//		managedJob.set
-//		
-//		return managedJob;
-//	}
-//	
-//	public void destroyUser(int id) {
-//		User user = em.find(User.class, id);
-//		em.remove(user);
-//	}
-//	public void destroyJob(int id, int tId) {
-//		String query = "Delete from Job where id =:tId";
-//		em.createQuery(query).setParameter("tId", tId).executeUpdate();
-//	}
+	public List<User> indexUsers() {
+		String query = "Select u from User u";
+		return em.createQuery(query, User.class).getResultList();
+	}
+	
+	public List<Job> indexJobs() {
+		String query = "Select j from Job j";
+		return em.createQuery(query, Job.class).getResultList();
+	}
+		
+	public User showUser(int id) {
+		return em.find(User.class, id);
+	} 
+	
+	public Collection<Job> showJob(int id) {
+		User user = null;
+		try{
+			user = em.find(User.class, id);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		if(user != null) {
+			return user.getJobs();
+		}
+		return null;
+	}
+	
+	public void createJob(int id, Job job) {
+		User user = em.find(User.class, id);
+		job.setUser(user);
+		em.persist(job);
+		em.flush();
+	}
+	public User create (User user) {
+		String rawPassword = user.getPassword();
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		user.setPassword(encodedPassword);
+		em.persist(user);
+		em.flush();
+		return user;	
+	}
+	public User authenticateUser (User loginData) throws Exception {
+		User user = em.createQuery("select u from User u where u.username = :username", User.class)
+					.setParameter("username", loginData.getUsername())
+					.getSingleResult();
+		if(passwordEncoder.matches(loginData.getPassword(), user.getPassword())) {
+			return null;
+		}
+		return null;
+	}
+	
+	public User update(int id, User user) {
+		User updatedUser = em.find(User.class, id);
+		updatedUser.setUsername(user.getUsername());
+		updatedUser.setPassword(user.getPassword());
+		updatedUser.setEmail(user.getEmail());
+		updatedUser.setFname(user.getFname());
+		updatedUser.setLname(user.getLname());
+		updatedUser.setLocation(user.getLocation());
+		updatedUser.setJobs(user.getJobs());
+	
+		em.merge(updatedUser);
+		return updatedUser;
+	}
+	
+	public Job update(int id, Job job) {
+		User managedUser = em.find(User.class, id);
+		job.setUser(managedUser);
+		Job managedJob = em.find(Job.class, id);
+		managedJob.setAppDate(job.getAppDate());
+		managedJob.setCity(job.getCity());
+		managedJob.setCompanyName(job.getCompanyName());
+		managedJob.setContactEmail(job.getContactEmail());
+		managedJob.setContactFname(job.getContactFname());
+		managedJob.setContactLname(job.getContactLname());
+		managedJob.setContactPhone(job.getContactPhone());
+		managedJob.setDesiredSalary(job.getDesiredSalary());
+		managedJob.setOfferedSalary(job.getOfferedSalary());
+		managedJob.setJobTitle(job.getJobTitle());
+		managedJob.setNotes(job.getNotes());
+		managedJob.setOffer(job.getOffer());
+		managedJob.setPostingUrl(job.getPostingUrl());
+		managedJob.setState(job.getState());
+		managedJob.setSalType(job.getSalType());
+		managedJob.setStartDate(job.getStartDate());
+		
+		return managedJob;
+	}
+	
+	public void destroyUser(int id) {
+		User user = em.find(User.class, id);
+		em.remove(user);
+	}
+	public void destroyJob(int id, int jId) {
+		String query = "Delete from Job where id =:jId";
+		em.createQuery(query).setParameter("jId", jId).executeUpdate();
+	}
 }
