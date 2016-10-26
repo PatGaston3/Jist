c57fd0a3501bd1bbed93a39eb438c991dd8c1451
Steve Zuber <stevenjzuber@gmail.com>
2016-10-26 11:18:08 -0600

    added updates sql

--- JistMVC/jist.sql
+++ JistMVC/jist.sql
@@ -5,15 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema Jist
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `Jist` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema Jist
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `Jist` DEFAULT CHARACTER SET utf8 ;
+USE `Jist` ;
 
 -- -----------------------------------------------------
 -- Table `user`
@@ -72,6 +72,7 @@ SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE USER 'user' IDENTIFIED BY 'password';
 
 GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'user';
+GRANT SELECT, INSERT, TRIGGER ON TABLE * TO 'user';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
@@ -81,8 +82,9 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `mydb`;
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `fname`, `lname`, `location`) VALUES (1, 'test', 'password', 'test@test.com', 'testy', 'mctestface', 'denver');
+USE `Jist`;
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `fname`, `lname`, `location`) VALUES (1, 'user1', 'user1', 'user1@user1.com', 'Testy', 'McTestface', 'Denver');
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `fname`, `lname`, `location`) VALUES (2, 'user2', 'user2', 'steve@test.com', 'Steve', 'Lastname', 'Chicago');
 
 COMMIT;
 
@@ -91,8 +93,12 @@ COMMIT;
 -- Data for table `job`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `mydb`;
-INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (1, 1, 'white_house', 'president', 'washington', 'dc', '10/26/16', 'barack', 'obama', '123-456-7890', 'barry@obams.com', 1, 100000000, 65000, 'https://www.youtube.com/watch?v=WHWmOEVd63E', 'year', 'none', '01/01/17');
+USE `Jist`;
+INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (1, 1, 'Vandelay Industries', 'Vice President', 'Seattle', 'WA', '10/26/16', 'Art', 'Vandelay', '555-1234', 'art@vandelayind.com', 1, 250000, 180000, 'urlhereforvandelay', 'year', 'notes go here', '01/01/17');
+INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (2, 1, 'Reddit', 'Director of Marketing', 'San Francisco', 'CA', '10/27/16', 'Guy', 'LeDouche', '555-7890', 'guy@reddit.com', 0, 175000, 185000, 'urlhereforreddit', 'year', 'notes go here', '01/15/17');
+INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (3, 1, 'TestCo', 'TestPosition', 'TestCity', 'TestState', '10/20/16', 'TestFName', 'TestLName', '555-TEST', 'test1@test.com', 1, 100000, 100000, 'testurl1', 'contract', 'test notes for user 1', '01/30/17');
+INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (4, 2, 'TestCo', 'TestPosition', 'TestCity', 'TestState', '10/20/16', 'TestFName', 'TestLName', '555-TEST', 'test2@test.com', 0, 50000, 75000, 'testurl2', 'year', 'test notes for user 2', '12/31/16');
+INSERT INTO `job` (`id`, `user_id`, `company_name`, `job_title`, `city`, `state`, `app_date`, `contact_fname`, `contact_lname`, `contact_phone`, `contact_email`, `offer`, `desired_salary`, `offered_salary`, `posting_url`, `sal_type`, `notes`, `start_date`) VALUES (5, 2, 'Marsh Initiative', 'Key Account Manager', 'South Park', 'CO', '10/01/16', 'Randy', 'Marsh', '555-1010', 'randy@marshco.com', 1, 3000, 3000, 'urlhereformarsh', 'month', 'notes go here', '01/01/17');
 
 COMMIT;
 
